d18f2895fce219ae70d228d425b903c08dc21ec6
Pulkit Kathuria <kevincobain2000@gmail.com>
2024-06-30 05:12:08 +0900

    Update pkg/slices.go
    Co-authored-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>

--- pkg/slices.go
+++ pkg/slices.go
@@ -5,7 +5,7 @@ type SliceFlags []string
 func (i *SliceFlags) String() string {
 	return ""
 }
-func (i *SliceFlags) Set(value string) error {
+func (i *SliceFlags) Append(value string) error {
 	*i = append(*i, value)
 	return nil
 }
